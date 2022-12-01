ac0874eff3fd6a922afa321beb03d6ca4a31ecc3
snwooten <snwooten@gmail.com>
2022-12-01 15:24:15 -0500

    Add handling for getting and cacheing the inputs

---
+++ inputs/get_input.ts
@@ -0,0 +1,51 @@
+
+import fetch from "node-fetch";
+import { writeFileSync, existsSync } from "fs"
+
+function buildInputUrl(day: string): string {
+    const url = `https://adventofcode.com/2022/day/${day}/input`;
+    return url;
+} 
+
+function buildFileName(day: string): string {
+    return `./inputs/day_${day}_input.txt`
+}
+
+ function checkIfInputExists(day): boolean { 
+    const fileName = buildFileName(day);
+    const inputExists = existsSync(fileName)
+    
+    return inputExists;
+}
+
+export function getInput(day: string): void {
+    const inputExists = checkIfInputExists(day);
+
+    if (!inputExists) {
+        const url = buildInputUrl(day);
+        fetch(url, {
+        headers: {
+            "cookie": process.env.COOKIE,
+            "User-Agent": "https://github.com/snwooten/advent_of_code_2022 by snwooten@gmail.com",
+        },
+        })
+        .then((res) => {
+            console.log("Getting advent calendar data...");
+            if (res.status !== 200) {
+                throw new Error(String(res.status))
+            }
+            return res.text();
+        })
+        .then((body) => {
+            const fileName = buildFileName(day);
+            writeFileSync(`./day_${day}_input.txt`, body.replace(/\n$/, ""))
+            console.log('SAVED!');
+        })
+        .catch((exception) => {
+            console.log("Exception : ", exception);
+        });
+    }
+
+}
+
+getInput('2');
